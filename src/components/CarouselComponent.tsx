a0ee0e646e873a9de65ca77a0bb7f733a971dbc2
Snowbeast138 <snowbeast138@gmail.com>
2023-08-19 20:52:56 -0600

    Video remplace

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -1,62 +1,65 @@
-import React, { useState, useEffect } from "react";
-import { Carousel, CarouselResponsiveOption } from "primereact/carousel";
-import { Image } from "primereact/image";
-import ImagesJSON from "../mocks/imagesUrl.json";
+import React from "react";
+// import { Carousel, CarouselResponsiveOption } from "primereact/carousel";
+// import { Image } from "primereact/image";
+// import ImagesJSON from "../mocks/imagesUrl.json";
+import ReactPlayer from "react-player";
 
 function CarouselComponent() {
-  interface Image {
-    name: string;
-  }
+  // interface Image {
+  //   name: string;
+  // }
 
-  const [images, setImages] = useState<Image[]>([]);
+  // const [images, setImages] = useState<Image[]>([]);
 
-  useEffect(() => {
-    setImages(ImagesJSON);
-  }, []);
+  // useEffect(() => {
+  //   setImages(ImagesJSON);
+  // }, []);
 
-  const responsiveOptions: CarouselResponsiveOption[] = [
-    {
-      breakpoint: "1199px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "1199px",
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "1199px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-  ];
+  // const responsiveOptions: CarouselResponsiveOption[] = [
+  //   {
+  //     breakpoint: "1199px",
+  //     numVisible: 1,
+  //     numScroll: 1,
+  //   },
+  //   {
+  //     breakpoint: "1199px",
+  //     numVisible: 2,
+  //     numScroll: 1,
+  //   },
+  //   {
+  //     breakpoint: "1199px",
+  //     numVisible: 1,
+  //     numScroll: 1,
+  //   },
+  // ];
 
-  const productTemplate = (image: Image) => {
-    return (
-      <div
-        className="mb-3"
-        style={{
-          backgroundImage: `url(${image.name})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-        }}
-      >
-        {/* <Image
-          src={image.name}
-          preview
-          width="100vw"
-          height="100vh"
-          alt="Image"
-        /> */}
-        {/* <img src={image.name} alt={image.name} /> */}
-      </div>
-    );
-  };
+  // const productTemplate = (image: Image) => {
+  //   return (
+  //     <div className="mb-3">
+  //       <Image
+  //         src={image.name}
+  //         preview
+  //         width="95vw"
+  //         height="80vh"
+  //         alt="Image"
+  //       />
+  //       {/* <img src={image.name} alt={image.name} /> */}
+  //     </div>
+  //   );
+  // };
 
   return (
     <div className="card">
-      <h1>Aprende de Redes</h1>
+      <ReactPlayer
+        url={"https://www.youtube.com/embed/NvdvxTB9p9E"}
+        controls
+        width="100%"
+        height="80vh"
+        muted
+        loop
+        playing={true}
+      />
+      {/* <h1>Aprende de Redes</h1>
       <Carousel
         value={images}
         numVisible={1}
@@ -65,7 +68,7 @@ function CarouselComponent() {
         itemTemplate={productTemplate}
         // circular
         // autoplayInterval={3000}
-      />
+      /> */}
     </div>
   );
 }
